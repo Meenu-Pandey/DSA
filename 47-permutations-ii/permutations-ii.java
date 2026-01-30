74fcefd5fff999285a1386aeac1823fa66264a45
Meenu Pandey <pandeymeenu057@gmail.com>
2026-01-30 21:39:07 +0530

    Time: 3 ms (91.89%) | Memory: 46.9 MB (29.75%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -2,28 +2,30 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> temp=new ArrayList<>();
-        Set<List<Integer>> distinct=new HashSet<>();
-        Set<Integer> visited=new HashSet<>();
-        helper(0, nums, distinct, visited, temp);
-        res.addAll(distinct);
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int num:nums)
+        {
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        helper(nums, res, map, temp);
         return res;
     }
 
-    public void helper(int i, int[] nums, Set<List<Integer>> distinct, Set<Integer> visited, List<Integer> temp) {
-        if(i==nums.length)
+    public void helper(int[] nums, List<List<Integer>> res, Map<Integer, Integer> map, List<Integer> temp) {
+        if(temp.size()==nums.length)
         {
-            distinct.add(new ArrayList<>(temp));
+            res.add(new ArrayList<>(temp));
             return;
         }
-        for(int k=0;k<nums.length;k++)
+        for(int num:map.keySet())
         {
-            if(!visited.contains(k))
+            if(map.get(num)>0)
             {
-                visited.add(k);
-                temp.add(nums[k]);
-                helper(i+1, nums, distinct, visited, temp);
+                temp.add(num);
+                map.put(num, map.get(num)-1);
+                helper(nums, res, map, temp);
                 temp.remove(temp.size()-1);
-                visited.remove(k);
+                map.put(num, map.get(num)+1);
             }
         }
     }
