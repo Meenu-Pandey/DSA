c233b26e413f6051787784c992c53a6140e124a3
Meenu Pandey <pandeymeenu057@gmail.com>
2026-02-12 20:20:11 +0530

    Time: 5 ms (42.31%) | Memory: 50.1 MB (25.10%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.java
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -1,24 +1,22 @@
-class Solution {
-    List<String> ans = new ArrayList<>();
-    String str;
+public class Solution {
     public List<String> restoreIpAddresses(String s) {
-        str = s;
-        magical("", 0, 0);
-        return ans;
-    } 
-
-    void magical( String path, int index, int dots) {
-        if (dots > 4) return;
-        if (dots == 4 && index >= str.length()) {
-            ans.add(path.substring(0,path.length()-1));
-            return;
-        }
-        for (int length = 1; length <= 3 && index + length <= str.length(); length++) {
-            String num = str.substring(index, index + length);
-            if (num.charAt(0) == '0' && length != 1) break;
-            else if (Integer.parseInt(num) <= 255) {
-                magical( path + str.substring(index, index + length) + ".", index + length,dots + 1);
+        List<String> res = new ArrayList<String>();
+        int len = s.length();
+        for(int i = 1; i<4 && i<len-2; i++){
+            for(int j = i+1; j<i+4 && j<len-1; j++){
+                for(int k = j+1; k<j+4 && k<len; k++){
+                    String s1 = s.substring(0,i), s2 = s.substring(i,j), s3 = s.substring(j,k), s4 = s.substring(k,len);
+                    if(isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4)){
+                        res.add(s1+"."+s2+"."+s3+"."+s4);
+                    }
+                }
             }
         }
+        return res;
+    }
+    public boolean isValid(String s){
+        if(s.length()>3 || s.length()==0 || (s.charAt(0)=='0' && s.length()>1) || Integer.parseInt(s)>255)
+            return false;
+        return true;
     }
 }
