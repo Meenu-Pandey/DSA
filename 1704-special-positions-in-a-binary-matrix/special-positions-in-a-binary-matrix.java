375e88daa7ddc0baa3577422b57e85395e5e4050
Meenu Pandey <pandeymeenu057@gmail.com>
2026-03-04 21:41:51 +0530

    Time: 2 ms (94.84%) | Memory: 47.2 MB (44.84%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -16,7 +16,7 @@ class Solution {
         int ans = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j] == 1 & row[i] == 1 & col[j] == 1){
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1){
                     ans++;
                 }
             }
